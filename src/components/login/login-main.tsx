69519589f5c4725ffb165a2d574ecc307fba4a5b
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2024-09-07 13:32:23 -0300

    feat: update login page w new images, texts colors

--- src/components/login/login-main.tsx
+++ src/components/login/login-main.tsx
@@ -12,15 +12,12 @@ export function LoginMain(): JSX.Element {
       <div className='relative hidden items-center justify-center  lg:flex'>
         <NextImage
           imgClassName='object-cover'
-          blurClassName='bg-accent-blue'
-          src='/assets/twitter-banner.png'
+          blurClassName='bg-accent-pink'
+          src='/assets/fofoca-banner.webp'
           alt='Fofoca banner'
           layout='fill'
           useSkeleton
         />
-        <i className='absolute'>
-          <CustomIcon className='h-96 w-96 text-white' iconName='TwitterIcon' />
-        </i>
       </div>
       <div className='flex flex-col items-center justify-between gap-6 p-8 lg:items-start lg:justify-center'>
         <i className='mb-0 self-center lg:mb-10 lg:self-auto'>
@@ -34,7 +31,7 @@ export function LoginMain(): JSX.Element {
         <div className='flex max-w-xs flex-col gap-4 font-twitter-chirp-extended lg:max-w-none lg:gap-16'>
           <h1
             className='text-3xl before:content-["Aqui_você_pode_fofocar_a_vontade."] 
-                       lg:text-6xl lg:before:content-["Fofoca?_Rasga!"]'
+                       lg:text-6xl lg:before:content-["Aqui_você_pode_fofocar_a_vontade."]'
           />
           <h2 className='hidden text-xl lg:block lg:text-3xl'>
             Cadastre-se no Fofoca hoje.
@@ -48,23 +45,23 @@ export function LoginMain(): JSX.Element {
                          dark:hover:brightness-90 dark:focus-visible:brightness-90 dark:active:brightness-75'
               onClick={signInWithGoogle}
             >
-              <CustomIcon iconName='GoogleIcon' /> Inscreva-se no Google
+              <CustomIcon iconName='GoogleIcon' /> Inscreva-se com Google
             </Button>
             <Button
               className='flex cursor-not-allowed justify-center gap-2 border border-light-line-reply font-bold text-light-primary
                          transition hover:bg-[#e6e6e6] focus-visible:bg-[#e6e6e6] active:bg-[#cccccc] dark:border-0
                          dark:bg-white dark:hover:brightness-90 dark:focus-visible:brightness-90 dark:active:brightness-75'
             >
-              <CustomIcon iconName='AppleIcon' /> Inscreva-se na Apple
+              <CustomIcon iconName='AppleIcon' /> Inscreva-se com Apple
             </Button>
 
             <p
               className='inner:custom-underline inner:custom-underline text-center text-xs
-                         text-light-secondary inner:text-accent-blue dark:text-dark-secondary'
+                         text-light-secondary inner:text-accent-pink dark:text-dark-secondary'
             >
               Ao se inscrever, você concorda com os{' '}
               <a
-                href='https://twitter.com/tos'
+                href='https://google.com.br'
                 target='_blank'
                 rel='noreferrer'
               >
@@ -72,7 +69,7 @@ export function LoginMain(): JSX.Element {
               </a>{' '}
               e{' '}
               <a
-                href='https://twitter.com/privacy'
+                href='https://google.com.br'
                 target='_blank'
                 rel='noreferrer'
               >
@@ -80,7 +77,7 @@ export function LoginMain(): JSX.Element {
               </a>
               , incluindo{' '}
               <a
-                href='https://help.twitter.com/rules-and-policies/twitter-cookies'
+                href='https://google.com.br'
                 target='_blank'
                 rel='noreferrer'
               >
@@ -92,8 +89,8 @@ export function LoginMain(): JSX.Element {
           <div className='flex flex-col gap-3'>
             <p className='font-bold'>Já tem uma conta?</p>
             <Button
-              className='border border-light-line-reply font-bold text-accent-blue hover:bg-accent-blue/10
-                         focus-visible:bg-accent-blue/10 focus-visible:!ring-accent-blue/80 active:bg-accent-blue/20
+              className='border border-light-line-reply font-bold text-accent-pink hover:bg-accent-pink/10
+                         focus-visible:bg-accent-pink/10 focus-visible:!ring-accent-pink/80 active:bg-accent-pink/20
                          dark:border-light-secondary'
               onClick={signInWithGoogle}
             >
