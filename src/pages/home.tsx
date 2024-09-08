3de12aa579759f153dbce1925ab7adb3ea3bed96
Igor Sousa <igor.ssqwe@gmail.com>
2024-09-08 19:15:39 -0300

    Atualizar o home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -13,7 +13,7 @@ import { useInfiniteScroll } from '@lib/hooks/useInfiniteScroll';
 import { orderBy, where } from 'firebase/firestore';
 import { AnimatePresence } from 'framer-motion';
 import type { ReactElement, ReactNode } from 'react';
-import { UpdateUsername } from '@components/ui/home';
+import { UpdateUsername } from '@components/ui/home/update-username';
 
 export default function Home(): JSX.Element {
   const { isMobile } = useWindow();
