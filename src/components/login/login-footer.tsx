f8b3cd5c521123c60201d9469a2522e98eb925ec
igaosoza <eu@igaosoza.com>
2024-09-07 22:29:53 -0300

    fix: fofoca name

--- src/components/login/login-footer.tsx
+++ src/components/login/login-footer.tsx
@@ -35,7 +35,7 @@ export function LoginFooter(): JSX.Element {
             {linkName}
           </a>
         ))}
-        <p>© 2024 Fofoca</p>
+        <p>© 2024 Fofoca.me</p>
       </nav>
     </footer>
   );
